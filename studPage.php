81c2430da5e0e0728bc9e05f9e026e509dd2d04e
Artem <klufone@mail.ru>
2016-10-25 01:24:59 +0300

    first commit. Add studpage v0.1 and some description of using html

---
+++ studPage.php
@@ -0,0 +1,83 @@
+
+
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN">
+<html>
+<head>
+  <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+  <title>Электронный дневник</title>
+</head>
+<body>
+<table  width="100%" cellpadding="0" cellspacing="0" border="0" valign="top">
+<tr valign="top">
+	<td width="100" align="justify">
+	
+		<table  width="100%" cellpadding="0" cellspacing="0" border="0">
+		<tr valign="top">
+		<td bgcolor="#cccccc" width="100%">
+	
+	
+		<table width="100%" cellpadding="7" cellspacing="1" border="0" style="font-size: 10pt; font-family: tahoma;">
+		<tr>
+			<td bgcolor="white" colspan="2" align="center"><b>Информация об ученике</b></td>
+		</tr><tr>
+			<td bgcolor="white">Имя</td>
+			<td bgcolor="white">Егорка Епифанов</td>
+		</tr><tr>
+			<td bgcolor="white">Класс</td>
+			<td bgcolor="white"><a href=/tsPHP/index.php?more=23>11-А</a></td>
+		</tr><tr>			
+		</tr><tr>
+			<td bgcolor="white" colspan="2" align="center"><div style="font-size: 9pt;"><a href="/tsPHP/index.php?logout">Выйти</a></div></td>
+		</tr>
+		</table>
+
+		</td></tr>
+		</table>
+	</td>
+	
+	<td width="*" align="top">
+		<table width="98%" align="middle" cellpadding="0" cellspacing="0" border="0">
+		<tr><td bgcolor="#cccccc" width="100%">
+			<table width="100%" cellpadding="2" cellspacing="1" border="0" style="font-size: 9pt; font-family: tahoma;">
+			<tr>
+				<td width="8%" bgcolor="white">Дисциплина</td>
+				<td width="*%" bgcolor="white" align="center">Оценки</td>
+				<td width="20%" bgcolor="white">Преподаватель</td>
+			</tr>
+			<tr>
+				<td width="8%" bgcolor="white">Математика</td>
+				<td width="*%" bgcolor="white" align="center">None</td>
+				<td width="20%" bgcolor="white">Медведева <br> Людмила Николаевна </td>
+			</tr><tr>
+				<td width="8%" bgcolor="white">Русский язык</td>
+				<td width="*%" bgcolor="white" align="center">None</td>
+				<td width="20%" bgcolor="white">Королева <br> Мария Михайловна </td>
+			</tr><tr>
+				<td width="8%" bgcolor="white">Литература</td>
+				<td width="*%" bgcolor="white" align="center">None</td>
+				<td width="20%" bgcolor="white">Королева <br> Мария Михайловна </td>
+			</tr><tr>
+				<td width="8%" bgcolor="white">История</td>
+				<td width="*%" bgcolor="white" align="center">None</td>
+				<td width="20%" bgcolor="white">Осипова <br> Мария Николаевна </td>
+			</tr><tr>
+				<td width="8%" bgcolor="white">Физкультура</td>
+				<td width="*%" bgcolor="white" align="center">None</td>
+				<td width="20%" bgcolor="white">Тараканов <br> Александр Александрович </td>
+			</tr><tr>
+				<td bgcolor="white" colspan="3" align="right">
+					<a href="/tsPHP/index.php?more=1" title = " Узнать подробнее об оценках и внести новые">Изменить
+					</a>
+				</td>
+			</tr>		
+			</table>
+		</td></tr>
+		</table>
+	</td>
+</tr>
+</table>
+
+</body>
+</html>
+
+

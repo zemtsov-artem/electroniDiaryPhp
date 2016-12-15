94fdacaebf968f316db7740297dba27e3e11b447
Artem <klufone@mail.ru>
2016-12-16 02:00:12 +0300

    add connect

--- UsingHtmlForms.php
+++ UsingHtmlForms.php
@@ -1,3 +1,26 @@
+<?php
+    //$dbh = new PDO('mysql:host=localhost:3306;dbname=electric_diary', root, Qwerty123);
+    #it is a way to fix os.x bug then we try to connect to localhost
+    $link = mysql_connect('127.0.0.1','root','Qwerty123');
+    if (!$link) {
+      die('Ошибка соединения: ' . mysql_error());
+    }
+    mysql_select_db('elec_diar');
+    $ath = mysql_query("select login from login_base;");
+    if($ath){
+      $passes = mysql_fetch_row($ath);
+      echo "<br>имя = ".$passes[1]."<br>";
+      echo "<br>имя = ".$passes[2]."<br>";
+    }
+
+  
+
+// Выполняем SQL-запрос
+
+
+
+?>
+
 <html>
  <head>
   <meta http-equiv="Content-Type"  content="text/html; charset=utf-8">
@@ -8,7 +31,7 @@
   <!-- Комментарий -->
   <p>Добро пожаловать.</p>
   <form action="action.php"  method="post" enctype="multipart/form-data">
-	Ваш логин : <input type="text" name="login " /><br />
+	Ваш логин  : <input type="text" name=$value /><br />
 	Ваш пароль: <input type="text" name="password" /><br />
 	<input type="submit" value="Отправить форму" />
    </form>
